651e1343cc95387ce4c40bde66f00b167ce08b26
Adela <you@example.com>
2016-06-23 14:09:22 -0700

    finished 5.4

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,4 +1,4 @@
-#Array
+##Array
 favorite_color = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
 
 favorite_color.each do |color| 
@@ -6,7 +6,7 @@ favorite_color.each do |color|
 end
 
 favorite_color.map! do |color|
-  color.upcase
+ color.upcase
 
 end 
   p favorite_color
@@ -22,3 +22,42 @@ usa.each do | state, capital |
 end
 
 p usa
+
+
+
+#Array
+letters = ["a", "b", "c", "d", "e"]
+p letters
+#Hash
+groups = { One: "solo", Two: "pair", Three: "trio", Four: "quartet"}
+p groups
+
+
+# 1) A method that iterates through the items, deleting any that meet a certain condition (for example, deleting any numbers that are less than 5).
+letters.delete_if { | letter| letter != "a" }
+
+p letters
+
+groups.delete_if { |number, name| number == :One}
+
+p groups
+
+# 2) A method that filters a data structure for only items that do satisfy a certain condition (for example, keeping any numbers that are less than 5).
+
+letters.keep_if { | letter| letter == "a" }
+p letters
+
+groups.keep_if { |number, name| number == :One}
+p groups
+
+
+# 3) A different method that filters a data structure for only items satisfying a certain condition -- Ruby offers several options!
+letters.select! { |letter| letter == "a" }
+
+p letters
+
+groups.select! { |number, name| number == :One}
+p groups
+
+
+
