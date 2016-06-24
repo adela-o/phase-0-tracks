f38282a0f67b1100124bb12833e63f2b6d2adec3
Adela <you@example.com>
2016-06-23 19:13:12 -0700

    build nested data structure

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,44 @@
+tv_shows = {
+    greys_anatomy: {
+        nickname: "Grey's",
+        show_info: {
+            num_of_seasons: 12,
+            basic_cable: yes
+        },
+        quotes: [
+            "It's a beautiful day to save lives", 
+            "You're my person"
+        ]
+    },
+    the_big_bang_theory: {
+        nickname: 'TBBT',
+        show_info: {
+            num_of_seasons: 9,
+            basic_cable: yes
+        },
+        quotes: [
+            "Soft kitty, warm kitty, little ball of fur,
+            happy kitty, sleepy kitty, pur, pur, pur", 
+            "Bazinga"
+        ]
+    },
+    game_of_thrones: {
+        nickname: 'GOT',
+        show_info: {
+            num_of_seasons: 6,
+            basic_cable: no
+        },
+        quotes: [
+            "Winter is coming"
+            "I drink and I know things"
+        ]
+    },
+    orange_is_the_new_black: {
+        nickname: 'OITNB',
+        show_info: {
+            num_of_seasons: 4,
+            basic_cable: no
+        },
+        quotes: []
+    }
+}
