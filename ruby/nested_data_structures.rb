08deca4edd3b69d117b168e1758abbf208b9abbe
Adela <you@example.com>
2016-06-23 19:27:18 -0700

    add test prints, fix minor bugs(add a comma on line 33, change basic_cable value to a boolean

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,9 +1,10 @@
+#build nested data structure
 tv_shows = {
     greys_anatomy: {
         nickname: "Grey's",
         show_info: {
             num_of_seasons: 12,
-            basic_cable: yes
+            basic_cable: true
         },
         quotes: [
             "It's a beautiful day to save lives", 
@@ -14,7 +15,7 @@ tv_shows = {
         nickname: 'TBBT',
         show_info: {
             num_of_seasons: 9,
-            basic_cable: yes
+            basic_cable: true
         },
         quotes: [
             "Soft kitty, warm kitty, little ball of fur,
@@ -26,10 +27,10 @@ tv_shows = {
         nickname: 'GOT',
         show_info: {
             num_of_seasons: 6,
-            basic_cable: no
+            basic_cable: false
         },
         quotes: [
-            "Winter is coming"
+            "Winter is coming",
             "I drink and I know things"
         ]
     },
@@ -37,8 +38,27 @@ tv_shows = {
         nickname: 'OITNB',
         show_info: {
             num_of_seasons: 4,
-            basic_cable: no
+            basic_cable: false
         },
         quotes: []
     }
 }
+
+#Print pieces
+# 1.
+tv_shows[:greys_anatomy][:nickname]
+
+# 2.
+tv_shows[:the_big_bang_theory][:quotes]
+
+# 3.
+tv_shows[:the_big_bang_theory][:quotes][0]
+
+# 4.
+tv_shows[:game_of_thrones][:quotes].push("Dracarys")
+
+# 5.
+tv_shows[:greys_anatomy][:quotes].reverse
+
+# 6.
+tv_shows[:orange_is_the_new_black][:nickname].downcase
