c920cacc7857dd8b44f81dc48c428f9a022aebad
Adela <you@example.com>
2016-06-26 14:36:02 -0700

    Scrapped previous work. Started over.

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -8,16 +8,64 @@
 #So 'a' would become 'e', 'u' would become 'a', 
 #and 'd' would become 'f'.
 
-#use .capitalize! to Modifies str by converting the first character 
-#to uppercase and the remainder to lowercase. 
-#Returns nil if no changes are made
+#Release 1: Provide a User Interface
+#Provide a user interface that lets a user enter a name 
+#and get a fake name back. Let the user do this repeatedly 
+#until they decide to quit by typing 'quit'. 
+#(They might just hit Enter to continue.)
 
+def alias_maker(name)
+vowels = ["a", "e", "i", "o", "u"]
+consonants= ["b", "c", "d", "f", "g", "h","j","k","l","m","n","p","q","r","s","t","v","w","x","y","z"]
+new_alias = name.split(" ").rotate
+swapped_name = new_alias.join(" ")
+letters = swapped_name.downcase.split("")
 
-#1) swap first and last name
-def name_swap(name1, name2)
-	name2 + name1
+	letters.map! do |letter|
+		if letter == " "
+			letter = " "
+		elsif letter == "z"
+			letter = "b"
+		elsif letter == "u"
+			letter = "a"
+		elsif vowels.include?(letter) || consonants.include?(letter)
+			if vowels.include?(letter)
+				letter = vowels[vowels.index(letter) +1]
+			else
+				letter = consonants[consonants.index(letter) +1]
+			end
+		else
+			puts "Please enter a valid character."
+		end
+	end
+	new_array = letters.join("")
+	new_array.split(" ").map! {|word|	word.capitalize}.join(" ")
+
 end
 
-puts name_swap(" adela", "oseguera")
 
 
+puts "Please enter a name you would like to convert. When done entering names please type 'quit'."
+name = gets.chomp
+name_array = []
+alias_array = []
+loop do
+	if name == "quit"
+		break
+	else
+		name_array << name
+		alias_array << alias_maker(name)
+	end
+	puts "Anymore names to enter?"
+	name = gets.chomp
+end
+
+#Release 2: Use a data structure to store the fake names as they are entered. 
+#When the user exits the program, 
+#iterate through the data structure and print all of the data the user entered.
+
+i = 0
+while i < alias_array.length
+	puts " #{alias_array[i]} is actually #{name_array[i]}."
+	i += 1
+end
