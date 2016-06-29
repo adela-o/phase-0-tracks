a1c1cbaaa78b2257e1cc355f5f9e2a3011635811
Adela <you@example.com>
2016-06-29 09:06:27 -0700

    adding methods to puppy class

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,9 +1,25 @@
 class Puppy
 
-  def fetch(toy)
+   def fetch(toy)
     puts "I brought back the #{toy}!"
-    toy
   end
 
+  def speak(w)
+    puts "Woof!" * w
+  end
+  
+  def  roll_over
+    p "*rolls over*"
+  end
+  
+  def dog_years(x)
+    p x * 7
+  end
+  
+    def initialize
+    puts "Initializing new puppy"
+  end
+  
 end
 
+spot = Puppy.new
