2646ed17f00287b5e3286050a61c9bfa96c85920
Adela <you@example.com>
2016-07-04 14:01:50 -0700

    make a mixin version of Shout

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,13 +1,33 @@
+#	module Shout
+#	  def self.yell_angrily(words)
+#	    words + "!!!" + " :("
+#	  end
+	  # add yelling happily
+#	  def self.yelling_happily(words)
+#	  	words + "!!!" + ":D"
+#	  end
+#	end
+
+	# Driver Code
+#	puts Shout.yell_angrily("ARRRGGGGHHHH!")
+#	puts Shout.yelling_happily("Yippe")
+
+#RELEASE 3: 
+# 1) Write two classes representing anything that might shout, 
+# and include the Shout module in those classes.
+# 2) Test your work by adding driver code at the bottom of the 
+# file that instantiates instances of your classes and calls the 
+# two module methods on each instance.
+
 module Shout
-  def self.yell_angrily(words)
-    words + "!!!" + " :("
-  end
-  # add yelling happily
-  def self.yelling_happily(words)
-  	words + "!!!" + ":D"
-  end
+	def yell_loudly(words)
+		"#{words.upcase}!"
+	end
+
+	def yell_excitedly(words)
+		"#{words.swapcase}!!!!!!!"		
+	end
+	
+
 end
 
-# Driver Code
-puts Shout.yell_angrily("ARRRGGGGHHHH!")
-puts Shout.yelling_happily("Yippe")
