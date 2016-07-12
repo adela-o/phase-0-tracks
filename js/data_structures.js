36e033e6b32aab730b46fcf3296b58b963609891
Adela <you@example.com>
2016-07-12 14:05:47 -0700

    update constructor function

--- js/data_structures.js
+++ js/data_structures.js
@@ -15,20 +15,37 @@
 
 // console.log(horse);
 
-// constructor function
+// Create car object
+
 var car = {
-	make: 'Toyota', 
-	'year': 2016, 
-	stickShift: true, 
-    gearShift: function() { 
-    	if (this.stickShift) {
+	make: "Toyota",
+	'year': 2016,
+	stickShift: true,
+};
+
+    function Car(make, year, stickShift) { 
+    	
+    	console.log("Here's your new car:", this);
+    	
+    	this.make = make;
+    	this.year = year;
+    	this.stickShift = stickShift;
+    	
+    	this.shift = function() { 
+    		if (this.stickShift) {
     		console.log("Change gears");
     	} else {
     		console.log("Change gears automatically");
     	}
-    }
-};
+    	};
+    console.log("CAR INITIALIZATION COMPLETE");
+}
 
-car.gearShift();
-car.stickShift = false;
-car.gearShift();
+console.log("Let's build a car...");
+
+var truck = new Car("Chevy", 1958, false);
+console.log(truck);
+truck.shift();
+console.log("-----");
+
+//console.log("Let's ");
