8d313554dd81b827a242f73e4587e0faf0e4fe85
Adela <you@example.com>
2016-07-12 13:25:04 -0700

    write constructor function

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,16 +1,34 @@
-var colors = ["Blue", "Periwinkle", "Onyx", "Marigold"];
-var name = ["Ponyta", "Rapidash", "Shadowfax", "Horsay"];
+// var colors = ["Blue", "Periwinkle", "Onyx", "Marigold"];
+// var name = ["Ponyta", "Rapidash", "Shadowfax", "Horsay"];
 
-colors.push("Magenta");
-name.push("Seabiscuit");
+// colors.push("Magenta");
+// name.push("Seabiscuit");
 
-//console.log(colors);
-//console.log(name);
+// console.log(colors);
+// console.log(name);
 
-var horse = {};
+// var horse = {};
 
-for ( var i = 0; i < name.length; i++){
-	horse[name[i]] = colors[i];
-}
+// for ( var i = 0; i < name.length; i++){
+// 	horse[name[i]] = colors[i];
+// }
 
-console.log(horse);
+// console.log(horse);
+
+// constructor function
+var car = {
+	make: 'Toyota', 
+	'year': 2016, 
+	stickShift: true, 
+    gearShift: function() { 
+    	if (this.stickShift) {
+    		console.log("Change gears");
+    	} else {
+    		console.log("Change gears automatically");
+    	}
+    }
+};
+
+car.gearShift();
+car.stickShift = false;
+car.gearShift();
