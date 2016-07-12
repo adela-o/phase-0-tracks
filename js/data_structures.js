b09a0657c96f2cb1646c60e5a8cdf97b0d54e086
Adela <you@example.com>
2016-07-12 13:09:58 -0700

    release 1

--- js/data_structures.js
+++ js/data_structures.js
@@ -3,3 +3,14 @@ var name = ["Ponyta", "Rapidash", "Shadowfax", "Horsay"];
 
 colors.push("Magenta");
 name.push("Seabiscuit");
+
+//console.log(colors);
+//console.log(name);
+
+var horse = {};
+
+for ( var i = 0; i < name.length; i++){
+	horse[name[i]] = colors[i];
+}
+
+console.log(horse);
