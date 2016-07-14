da6c7f6068e215eda49498054edb37949177c5c5
Adela Oseguera <imnosouperman@gmail.com>
2016-07-13 21:31:03 -0700

    Update vampires.rb
    REWORKED.

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -5,83 +5,85 @@ puts "How many employees will be processed today?"
 employees = gets.chomp.to_i
 
 while employee_num < employees
-	puts "What is your name?"		
-	name = gets.chomp		
+
+	puts "What is your name?"
+	name = gets.chomp
 	
- 	puts "How old are you?"		
- 	age = gets.chomp.to_i		
- 				
- 	puts "What year were you born?"		
-	birth_year = gets.chomp.to_i			
-	current_year = 2016		
-	age_reported = current_year - birth_year
-	# need to account for data that will be submitted if people haven't had a birthday yet.
+	puts "How old are you?"
+	age = gets.chomp.to_i
 	
-	puts "Our cafeteria serves garlic bread. Should we order some for you? (Y/N)"
-	garlic_bread = gets.chomp.downcase
-		case garlic_bread
-			when 'y'
-				garlic_bread = true
-			when 'n'
-				garlic_bread = false
+	puts "What year were you born in?"
+	birth_year = gets.chomp.to_i
+	
+	#way to compare birth year with age
+	current_year = 2016
+	calc_age = current_year - birth_year
+	
+	#age check
+	if calc_age != age 
+		age_correct = false
+	else
+		age_correct = true
+	end
+	
+	puts "Do you like garlic bread? (y/n)"
+	g_bread = gets.chomp.downcase
+	#turn input into boolean
+	case g_bread
+		when 'y'
+			g_bread = true 
+		when 'n'
+			g_bread = false
+		end
+	
+	puts "Would you like to sign up for health insurance? (y/n)"
+	h_ins = gets.chomp.downcase
+	#turn input into boolean
+	case h_ins
+		when 'y'
+			h_ins = true
+		when 'n'
+			h_ins = false 
 		end
 		
-	puts "Would you like to enroll in the company's health insurance? (Y/N)"		
- 	insurance = gets.chomp.downcase		
- 					
- 		case insurance 		
- 			when 'y'		
-				insurance = true					
-			when 'n'		
- 				insurance = false		
- 			end		
+	#Vamp conditions check 
+	result = case
+	
+		#Fast fail conditions check
+		when name == "Drake Cula" || name == "Tu Fang" then 
+			"Definitely a vampire"
+		
+		else
+			vamp_check = true
+			result = case
+				when vamp_check = age_correct && ( g_bread || h_ins ) then 
+				"Probably not a vampire."
+				when vamp_check = !age_correct && ( g_bread || h_ins) then
+			    "Probably a vampire"
+			    when vamp_check = !age_correct && ( !g_bread && !h_ins ) then
+			    	 "Almost certainly a vampire" 
+			    else
+			    	"Results inconclusive"
+			    end
+		end
+		
+	employee_num += 1
+	
+	allergies = ""
+	 puts "Please list your allergies. Type 'done' when finished"		
  						
- 		# Vampire conditions check				      		
- 	# Fast fail name check			
- 	if name == "Drake Cula" || name == "Tu Fang"		
- 		vamp_check = "Definitely a vampire."		
- 	else
- 	if (age_reported != 2016 - birth_year) || (age_reported != 2016 - (birth_year + 1))
- 		age_reported = false
- 	else
- 		age_reported = true
- 	end
- 	if age_reported  && garlic_bread || insurance		
- 		vamp_check = "Probably not a vampire."		
- 	end	
-				
- 	if !age_reported && (!garlic_bread || !insurance)		
- 		vamp_check = "Probably a vampire. Eek!"		
- 	end		
- 						
- 	if !age_reported && !garlic_bread  && !insurance		
- 		vamp_check = "Almost certainly a vampire! Halp!!"		
- 	end
- 	
- 	if vamp_check == "Probably not a vampire." || vamp_check == "Probably a vampire. Eek!" || vamp_check == "Almost certainly a vampire! Halp!!" || vamp_check == "Definitely a vampire."
- 		vamp_check = vamp_check
- 	else
- 		vamp_check = "Results inconclusive."
- 	end					
- 					
- puts "Please list your allergies. Type 'done' when finished"		
- allergies = ""		
- 						
- 	#allergy question asking loop		
- 	until allergies == "done" || allergies == "sunshine"		
+ 	#allergy question asking loop
+ 	allergy = []
+ 	until allergies == "done" ; "sunshine"		
  		allergies = gets.chomp.downcase		
- 			if allergies == "sunshine"		
- 				vamp_check = "Probably a vampire."	
- 			elsif allergies == "done"		
- 				puts "Allergies noted."		
- 			else		
- 				puts "Is that all?"		
- 			end		
- 	end		
- 	# end of allergy asking loop		
- 	end		
- 	employee_num += 1		
- 	puts vamp_check		
-end		
-  		  
- puts  "Actually, never mind! What do these questions have to do with anything? Let's all be friends."
+ 		allergy << allergies
+ 		if allergies.index('sunshine')
+ 			result = "Probably a vampire."
+ 			break
+ 		end
+  	end			
+
+puts result
+
+end
+puts  "Actually, never mind! What do these questions have to do with anything? Let's all be friends."
