5dc84985e805611b8cecfc8af29da4c4dff97562
Adela <you@example.com>
2016-07-26 16:25:58 -0700

    release 1

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -1,5 +1,14 @@
 console.log("The script is running!");
 
+// RELEASE: 1
+
+var myElement = document.createElement("p");
+var myNode = document.createTextNode("Cupcakes are life!!!!!");
+myElement.appendChild(myNode);
+var newElement = document.getElementById("about-cupcakes");
+newElement.appendChild(myElement);
+
+// RELEASE: 2
 function addPinkBorder(event) {
   console.log("click happened! here's the click event:");
   console.log(event);
@@ -8,6 +17,7 @@ function addPinkBorder(event) {
 
 var photo = document.getElementById("cupcake-photo");
 photo.addEventListener("click", addPinkBorder);
+
 
 
 
